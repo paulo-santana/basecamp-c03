efd18c7083d35c6a3bd46b7839d7e0b62d67f750
psergio- <psergio-@student.42sp.org.br>
2021-04-09 15:58:09 +0000

    ex01

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -1,33 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: psergio- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/08 23:12:02 by psergio-          #+#    #+#             */
-/*   Updated: 2021/04/09 14:13:32 by psergio-         ###   ########.fr       */
+/*   Created: 2021/04/09 13:11:15 by psergio-          #+#    #+#             */
+/*   Updated: 2021/04/09 15:38:18 by psergio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_strcmp(char *s1, char *s2)
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
+	unsigned int	counter;
 	unsigned char	c1;
 	unsigned char	c2;
-	int	diff;
-	while (*s1)
+	int				diff;
+
+	if (n == 0)
+		return (0);
+	counter = 0;
+	while (counter < n)
 	{
-		c1 = *s1;
-		c2 = *s2;
+		c1 = s1[counter];
+		c2 = s2[counter];
 		diff = c1 - c2;
 		if (diff != 0)
 			return diff;
-		s1++;
-		s2++;
+		counter++;
 	}
-	c1 = *s1;
-	c2 = *s2;
-	if (c1 != c2)
-		return *s1 - *s2;
 	return 0;
 }
