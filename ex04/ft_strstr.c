51b019bbe78996184de728cf1f944cd44ebf934c
psergio- <psergio-@student.42sp.org.br>
2021-04-10 13:13:11 +0000

    ft_strstr.c

--- ex04/ft_strstr.c
+++ ex04/ft_strstr.c
@@ -1,19 +1,30 @@
 char	*ft_strstr(char *str, char *to_find)
 {
-	int i = 0;
-	int	matched = 0;
-	char *match_start;
+	int		i = 0;
+	int		matched = 0;
+	int		to_find_pos = 0;
+	char	*match_start;
 
 	while (str[i] != 0)
 	{
 		to_find_pos = 0;
-		while (str[i + to_find_pos] == to_find[to_find_pos])
-		{
-			str[i] + to_find
+		match_start = &str[i];
+		while (to_find[to_find_pos] != '\0') {
+			if (to_find[to_find_pos] != match_start[to_find_pos])
+			{
+				matched = 0;
+				to_find_pos++;
+				break ;
+			}
+			else
+			{
+				matched = 1;
+				to_find_pos++;
+			}
 		}
+		if (matched)
+			return (match_start);
+		i++;
 	}
-	if (matched)
-		return (match_start);
-
 	return NULL;
 }
