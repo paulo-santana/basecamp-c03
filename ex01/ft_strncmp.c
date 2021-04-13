a3502a0db0f30f5b70d30e8e28b9e06cc2961b36
psergio- <psergio-@student.42sp.org.br>
2021-04-13 11:36:13 +0000

    fix ft_strncmp

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: psergio- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 13:11:15 by psergio-          #+#    #+#             */
-/*   Updated: 2021/04/12 12:29:40 by psergio-         ###   ########.fr       */
+/*   Updated: 2021/04/13 11:35:16 by psergio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	if (n == 0)
 		return (0);
 	counter = 0;
-	while (counter < n)
+	while (counter < n && (s1[counter] || s2[counter]))
 	{
 		c1 = s1[counter];
 		c2 = s2[counter];
